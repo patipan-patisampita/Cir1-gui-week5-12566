4dca0ab69f8ea8b6a263eb91c931c0f21f5ed6c1
patipan-patisampita <patipan.patisampita@gmail.com>
2023-06-13 13:04:28 +0700

    initial

---
+++ bin/dart_week5.dart
@@ -0,0 +1,11 @@
+void main(){
+  print("Assignment operators");
+  print("==========");
+  const int n1 = 11;
+  const int n2 = 5;
+  print("n1+n2 = ${n1 + n2}");//1.Add
+  print("n1-n2 = ${n1 - n2}"); //2.Subtraction = 5
+  print("n1*n2 = ${n1 * n2}");//3.Multication = 50
+  print("n1/n2 = ${n1 / n2}");//4.Division = 2.0
+  print("n1%n2 = ${n1 % n2}");//5.Modulus = 0
+}
